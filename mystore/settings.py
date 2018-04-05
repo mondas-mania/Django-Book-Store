9c53b4c92e1838dd2d7a47fd248f295a82c59541
David Sampson <sampsond1@cardiff.ac.uk>
2018-04-05 21:31:02 +0100

    Images?

--- mystore/settings.py
+++ mystore/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 INTERNAL_IPS = ['127.0.0.1']
 
