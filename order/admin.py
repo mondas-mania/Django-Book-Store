42f2e633c05dec57523f90d7ca317113ee2eaf81
David Sampson <sampsond1@cardiff.ac.uk>
2018-04-24 17:01:40 +0100

    Adding in order stuff in admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'card_number', 'cardholder_name', 'expiry_date', 'CVV_code', 'created', 'updated', 'paid']
+    list_filter = ['paid', 'updated', 'created', 'user']
+admin.site.register(Order, OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'book', 'price', 'quantity']
+admin.site.register(OrderItem, OrderItemAdmin)
