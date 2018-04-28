aeacf089d99f199f54d654982d9b41ae1305fa27
David Sampson <sampsond1@cardiff.ac.uk>
2018-04-28 04:06:08 +0100

    Doing good on pdfs

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.order_create, name='order_create'),
+    url(r'^pdf/(?P<order_id>[0-9]+)$', views.pdf, name='order_pdf'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from io import BytesIO
+from reportlab.pdfgen import canvas
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from account.models import Profile
 
 def order_create(request):
 	cart = Cart(request)
@@ -28,3 +31,34 @@ def order_create(request):
 			return render(request, 'order/order/create.html',{'form': form})			
 	else:
 		return render(request, 'order/order/create-login.html')
+
+
+def pdf(request, order_id=None):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="shipping_label.pdf"'
+
+    order = Order.objects.filter(id=order_id)[0]
+    user = User.objects.filter(id=order.user_id)[0]
+    if user != request.user:
+    	return HttpResponseRedirect("/")
+    profile = Profile.objects.filter(user_id=user.id)[0]
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    # Start writing the PDF here
+    p.drawString(100, 175, "SHIP TO:")
+    p.drawString(175, 175, str(user.first_name).upper() + " " + str(user.last_name).upper())
+    p.drawString(175, 150, str(profile.address).upper())
+    p.drawString(175, 125, str(profile.city).upper())
+    p.drawString(175, 100, str(profile.postcode).upper())
+    # End writing
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
